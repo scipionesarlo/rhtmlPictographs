5ce16caa71cc38ec89d4c89c8fa1b1c6d10160e0
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2016-04-17 14:54:12 +1000

    minor gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,34 @@ var widgetName = 'rhtmlPictographs';
 var _ = require('lodash');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var Promise = require('bluebird');
 
-gulp.task('clean', function(cb) {
+gulp.task('default', function () {
+  gulp.start('build');
+});
+
+//@TODO clean doesn't finish before next task ..
+//gulp.task('build', ['clean', 'compile-coffee', 'images', 'less', 'copy'], function () {
+gulp.task('build', ['compile-coffee', 'images', 'less', 'copy', 'makedocs'], function () {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('serve', ['connect', 'watch'], function () {
+  require('opn')('http://localhost:9000');
+});
+
+gulp.task('clean', function(done) {
+
   var fs = require('fs-extra');
-  fs.remove('dist', cb);
-  fs.remove('inst', cb);
+  Promise.promisifyAll(fs);
+
+  var deletePromises = []
+  deletePromises.push(fs.removeAsync('dist'));
+  deletePromises.push(fs.removeAsync('inst'));
+  deletePromises.push(fs.removeAsync('man'));
+  deletePromises.push(fs.removeAsync('R'));
+  Promise.all(deletePromises).then(done);
+  return true;
 });
 
 gulp.task('makedocs', function () {
@@ -24,7 +47,7 @@ gulp.task('less', function () {
   var less = require('gulp-less');
   return gulp.src('theSrc/styles/**/*.less')
     .pipe(less({}))
-    .pipe(gulp.dest('dist/browser/styles'))
+    .pipe(gulp.dest('browser/styles'))
     .pipe(gulp.dest('inst/htmlwidgets/lib/style'));
 });
 
@@ -33,7 +56,7 @@ gulp.task('compile-coffee', function () {
 
   gulp.src('theSrc/scripts/**/*.coffee')
     .pipe(gulp_coffee({ bare: true }))
-    .pipe(gulp.dest('dist/browser/scripts'))
+    .pipe(gulp.dest('browser/scripts'))
     .pipe(gulp.dest('inst/htmlwidgets/'));
 });
 
@@ -43,50 +66,54 @@ gulp.task('images', function () {
       progressive: true,
       interlaced: true
     })))
-    .pipe(gulp.dest('dist/browser/images'));
+    .pipe(gulp.dest('browser/images'));
 });
 
 gulp.task('copy', function () {
   gulp.src([
     'theSrc/**/*.html'
-  ], {}).pipe(gulp.dest('dist/browser'));
+  ], {}).pipe(gulp.dest('browser'));
 
   gulp.src([
     'resources/**/*.json'
-  ], {}).pipe(gulp.dest('dist/browser/resources'));
+  ], {}).pipe(gulp.dest('browser/resources'));
 
   var rename = require('gulp-rename');
-  gulp.src('htmlwidget.yaml')
+  gulp.src('theSrc/R/htmlwidget.yaml')
     .pipe(rename(widgetName + '.yaml'))
     .pipe(gulp.dest('inst/htmlwidgets/'));
+
+  gulp.src('theSrc/R/htmlwidget.R')
+    .pipe(rename(widgetName + '.R'))
+    .pipe(gulp.dest('R/'));
 
   var extLibs = [
     {
       src: 'node_modules/lodash/lodash.min.js',
       dest: [
         'inst/htmlwidgets/lib/lodash-4.6.1/',
-        'dist/browser/external/'
+        'browser/external/'
       ]
     },
     {
       src: 'node_modules/jquery/dist/jquery.min.js',
       dest: [
         'inst/htmlwidgets/lib/jquery-2.2.2/',
-        'dist/browser/external/'
+        'browser/external/'
       ]
     },
     {
       src: 'node_modules/d3/d3.min.js',
       dest: [
         'inst/htmlwidgets/lib/d3-3.5.16/',
-        'dist/browser/external/'
+        'browser/external/'
       ]
     },
     {
       src: 'node_modules/d3-grid/d3-grid.js',
       dest: [
         'inst/htmlwidgets/lib/d3-grid-0.1.2/',
-        'dist/browser/external/'
+        'browser/external/'
       ]
     }
   ]
@@ -110,8 +137,8 @@ gulp.task('connect', ['build'], function () {
   var serveIndex = require('serve-index');
   var app = require('connect')()
     .use(require('connect-livereload')({port: 35729}))
-    .use(serveStatic('dist/browser'))
-    .use(serveIndex('dist/browser'));
+    .use(serveStatic('browser'))
+    .use(serveIndex('browser'));
 
   require('http').createServer(app)
     .listen(9000)
@@ -120,32 +147,19 @@ gulp.task('connect', ['build'], function () {
     });
 });
 
-gulp.task('serve', ['connect', 'watch'], function () {
-  require('opn')('http://localhost:9000');
-});
-
 gulp.task('watch', ['connect'], function () {
   $.livereload.listen();
 
-  // watch for changes
+  // watch for changes in the browser directory and reload chrome on changes
   gulp.watch([
-    'dist/browser/**/*',
+    'browser/**/*',
   ]).on('change', $.livereload.changed);
 
+  // when these files change then do this,
+  // for example when the json file changes rerun the copy command
   gulp.watch('resources/**/*.json', ['copy']);
   gulp.watch('theSrc/**/*.html', ['copy']);
   gulp.watch('theSrc/images/**/*', ['images']);
   gulp.watch('theSrc/styles/**/*.less', ['less']);
   gulp.watch('theSrc/scripts/**/*.coffee', ['compile-coffee']);
-
-});
-
-//@TODO clean doesn't finish before next task ..
-//gulp.task('build', ['clean', 'compile-coffee', 'images', 'less', 'copy'], function () {
-gulp.task('build', ['compile-coffee', 'images', 'less', 'copy', 'makedocs'], function () {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
-
-gulp.task('default', function () {
-  gulp.start('build');
 });

bc5c9f9ef59509d317d82c83710ee54783d98589
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2016-06-29 08:09:52 +1000

    add examples for bugs and improvements

---
+++ examples/examples/bug_fixed_font_size.r
@@ -0,0 +1,6 @@
+#This file is auto generated from /examples/bug_fixed_font_size.html
+#The html demo examples and tutorial are the source of truth and are far more readable and beneficial. Start there (see readme for how).
+#Use these r files for reference, but know that some instructional content is not transferred from the html to the r examples files
+#TL;DR View the tutorial/example the way it was meant to be: in HTML format!
+
+rhtmlPictographs::graphic('{ "font-size": "10px", "font-family": "arial", "preserveAspectRatio": "none", "table": { "rows": [ [ "label:A", "label:A", "label:A", "label:A", "label:A", "label:A", "label:A", "label:A", "label:A", "label:A" ], [ "label:A" ], [ "label:A" ], [ "label:A" ], [ "label:A" ], [ "label:A" ], [ "label:A" ], [ "label:A" ], [ "label:A" ], [ "label:A" ] ] }} ')
---
+++ examples/examples/bug_lines_zindex.r
@@ -0,0 +1,6 @@
+#This file is auto generated from /examples/bug_lines_zindex.html
+#The html demo examples and tutorial are the source of truth and are far more readable and beneficial. Start there (see readme for how).
+#Use these r files for reference, but know that some instructional content is not transferred from the html to the r examples files
+#TL;DR View the tutorial/example the way it was meant to be: in HTML format!
+
+rhtmlPictographs::graphic('{ "table": { "lines": { "horizontal": [ 0, 0.5, 1], "vertical": [ 0, 1, 2] }, "rows": [ [ "square:lightblue", { "type" : "graphic", "value": { "padding": "3 3 3 3", "variableImage": "square:lightblue" } } ] ] } }')

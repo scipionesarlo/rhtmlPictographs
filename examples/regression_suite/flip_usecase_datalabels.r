b0bf517efd8eabd3a4e274c55de10f54bbe75295
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2017-03-18 13:21:44 +1000

    missed auto gen examples from previous checkin

---
+++ examples/regression_suite/flip_usecase_datalabels.r
@@ -0,0 +1,6 @@
+#This file is auto generated from /content/regression_suite/flip_usecase_datalabels.html
+#The html examples and tutorial are the source of truth and are far more readable and beneficial. Start there (see readme for how).
+#Use these r files for reference, but know that some instructional content is not transferred from the html to the r examples files
+#TL;DR View the tutorial/example the way it was meant to be: in HTML format!
+
+rhtmlPictographs::graphic('{ "proportion": 0.4, "numImages": 10, "numCols": 10, "text-footer": { "text": "40%", "font-size": "12.000000px", "font-weight": "normal", "font-family": "Arial", "font-color": "#2C2C2C", "horizontal-align": "center" }, "variableImage": "url:fromtop:#008080:/images/tree_grey.svg", "baseImage": "url:#CCCCCC:/images/tree_grey.svg", "width": 500, "height": 72.2064220183486, "background-color": "transparent", "columnGutter": 0, "rowGutter": 0.05, "padding": "0 0 0 0", "preserveAspectRatio": "xMidYMid", "resizable": "true"}')
---
+++ examples/regression_suite/flip_usecase_layout_rows.r
@@ -0,0 +1,6 @@
+#This file is auto generated from /content/regression_suite/flip_usecase_layout_rows.html
+#The html examples and tutorial are the source of truth and are far more readable and beneficial. Start there (see readme for how).
+#Use these r files for reference, but know that some instructional content is not transferred from the html to the r examples files
+#TL;DR View the tutorial/example the way it was meant to be: in HTML format!
+
+rhtmlPictographs::graphic('{ "proportion": 0.8, "numImages": 200, "numRows": 12, "text-header": { "text": "160", "font-size": "12.000000px", "font-weight": "normal", "font-family": "Arial", "font-color": "#2C2C2C", "horizontal-align": "center" }, "variableImage": "url:fromtop:#9E480E:/images/apple_grey.svg", "width": 340, "height": 271.493212669683, "background-color": "transparent", "columnGutter": 0, "rowGutter": 0, "padding": "0 0 0 0", "preserveAspectRatio": "none", "resizable": "false"}')

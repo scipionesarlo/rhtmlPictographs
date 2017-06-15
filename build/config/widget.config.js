9bd78acc4c111b8d084239c4f84a40c157423528
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2017-06-16 07:50:09 +1000

    use rhtmlBuildUtils v1.1.1

--- build/config/widget.config.js
+++ build/config/widget.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  widgetClassFile: 'theSrc/scripts/Pictograph.js',
   widgetEntryPoint: 'theSrc/scripts/rhtmlPictographs.js',
-  widgetName: 'rhtmlPictographs',
-  rFunction: 'rhtmlPictographs::graphic'
+  widgetName: 'rhtmlPictographs'
 }
